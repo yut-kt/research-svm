113b45981ac4c04f65be93a78c79960755b0fe81
yut-kt <yut.kt59@gmail.com>
2018-10-08 18:06:30 +0900

    add grid search

--- grid_search.py
+++ grid_search.py
@@ -14,45 +14,46 @@ def main():
     train_size = int(len(dataset['trains']) * 0.8)
     test_size = len(dataset['trains']) - train_size
     train_data, test_data, train_label, test_label = train_test_split(dataset['trains'],
-                                                                                      dataset['labels'],
-                                                                                      train_size=train_size,
-                                                                                      test_size=test_size)
+                                                                      dataset['labels'],
+                                                                      train_size=train_size,
+                                                                      test_size=test_size)
 
     tuned_parameters = [
-        {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
-        {'C': [1, 10, 100, 1000], 'kernel': ['rbf'], 'gamma': [0.001, 0.0001]},
-        {'C': [1, 10, 100, 1000], 'kernel': ['poly'], 'degree': [2, 3, 4], 'gamma': [0.001, 0.0001]},
-        {'C': [1, 10, 100, 1000], 'kernel': ['sigmoid'], 'gamma': [0.001, 0.0001]}
+        # {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+        {'C': [0.1, 1, 10, 100, 1000], 'kernel': ['rbf'], 'gamma': [0.001, 0.0001, 1e-3, 1e-4]},
+        # {'C': [1, 10, 100, 1000], 'kernel': ['poly'], 'degree': [2, 3, 4], 'gamma': [0.001, 0.0001]},
+        # {'C': [1, 10, 100, 1000], 'kernel': ['sigmoid'], 'gamma': [0.001, 0.0001]}
     ]
 
-    score = 'f1'
-    clf = GridSearchCV(
-        SVC(),
-        tuned_parameters,  # 最適化したいパラメータセット
-        cv=5,  # 交差検定の回数
-        scoring='%s_weighted' % score,  # モデルの評価関数の指定
-        n_jobs=-1
-    )
-    clf.fit(train_data, train_label)
+    scores = ['accuracy', 'precision', 'recall', 'f1']
+    for score in scores:
+        clf = GridSearchCV(
+            SVC(),
+            tuned_parameters,  # 最適化したいパラメータセット
+            cv=5,  # 交差検定の回数
+            scoring=score,  # モデルの評価関数の指定
+            n_jobs=-1
+        )
+        clf.fit(train_data, train_label)
 
-    print("# Tuning hyper-parameters for %s" % score)
-    print()
-    print("Best parameters set found on development set: %s" % clf.best_params_)
-    print()
+        print("# Tuning hyper-parameters for %s" % score)
+        print()
+        print("Best parameters set found on development set: %s" % clf.best_params_)
+        print()
 
-    # それぞれのパラメータでの試行結果の表示
-    print("Grid scores on development set:")
-    print()
-    for params, mean_score, scores in clf.grid_scores_:
-        print("%0.3f (+/-%0.03f) for %r"
-              % (mean_score, scores.std() * 2, params))
-    print()
+        # それぞれのパラメータでの試行結果の表示
+        print("Grid scores on development set:")
+        print()
+        for params, mean_score, scores in clf.grid_scores_:
+            print("%0.3f (+/-%0.03f) for %r"
+                  % (mean_score, scores.std() * 2, params))
+        print()
 
-    # テストデータセットでの分類精度を表示
-    print("The scores are computed on the full evaluation set.")
-    print()
-    y_true, y_pred = test_label, clf.predict(test_data)
-    print(classification_report(y_true, y_pred))
+        # テストデータセットでの分類精度を表示
+        print("The scores are computed on the full evaluation set.")
+        print()
+        y_true, y_pred = test_label, clf.predict(test_data)
+        print(classification_report(y_true, y_pred))
 
 
 if __name__ == '__main__':
